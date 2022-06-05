006388f4561f087df4c3d748fda3053d6aa797ee
Mokhtar Bacha <mokhtar@joinformal.com>
2022-06-05 02:36:02 +0200

    update

--- Formula/formal.rb
+++ Formula/formal.rb
@@ -2,11 +2,11 @@ class Formal < Formula
     desc "The Formal CLI"
     homepage "https://github.com/formalco/cli"
     url "https://formal-public.s3.eu-west-1.amazonaws.com/formal.tar.gz"
-    sha256 "79770064c6135bd47d5fb0cd7109bcd0787b7567a5394de57500ebc179fe271b"
-    version "0.1.0"
+    sha256 "d11b5e9bd12c8b21c7d72efca95a6dba5489a8ec7267a1c32429378546253462"
+    version "0.2.0"
   
     def install
-      bin.install "formalcli-darwin-arm64"
+      bin.install "formal"
     end
   end
   
