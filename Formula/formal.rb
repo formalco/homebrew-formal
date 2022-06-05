b4c40f09b4841a82cfade5a467155854c2d813db
Mokhtar Bacha <mokhtar@joinformal.com>
2022-06-05 20:22:28 +0200

    bump to v0.6.0

--- Formula/formal.rb
+++ Formula/formal.rb
@@ -1,12 +1,12 @@
 class Formal < Formula
     desc "The Formal CLI"
     homepage "https://github.com/formalco/cli"
-    version "0.5.0"
+    version "0.6.0"
 
     on_macos do
       if Hardware::CPU.intel?
         url "https://formal-public.s3.eu-west-1.amazonaws.com/release/formalcli-darwin-amd64.tar.gz"
-        sha256 "6f5bbe1714fc2499edd7e9919c6925aa757feaabd43d57ee73450cde9afb3470"
+        sha256 "77621af6df2ccce4944331275f9e099ab08644c2ceacfceef11f9dbdede54d60"
   
         def install
           bin.install "formal"
@@ -14,7 +14,7 @@ class Formal < Formula
       end
       if Hardware::CPU.arm?
         url "https://formal-public.s3.eu-west-1.amazonaws.com/release/formalcli-darwin-arm64.tar.gz"
-        sha256 "780a48e5beaafc5d11fd58ca0b6a8d8131415d34027869636ea1a03882071853"
+        sha256 "58cf90b8e190bc6485125571e3700df040b7dac7ea7d5e3d4fd913d4ac4cf1df"
   
         def install
           bin.install "formal"
@@ -25,12 +25,20 @@ class Formal < Formula
     on_linux do
       if Hardware::CPU.intel?
         url "https://formal-public.s3.eu-west-1.amazonaws.com/release/formalcli-linux-amd64.tar.gz"
-        sha256 "4d91c3e0cdb9910a149996ea3512274685bc570e97242f30f7a234d89e2305a0"
+        sha256 "c3245aa12316ecb5879a61554f15f25ad0a7a122ab5593672411a290deef7b55"
   
         def install
           bin.install "formal"
         end
       end
+
+      if Hardware::CPU.arm?
+        url "https://formal-public.s3.eu-west-1.amazonaws.com/release/formalcli-linux-arm64.tar.gz"
+        sha256 "262431dc45f1ff25796f7ac0139206fdcaaa19b70a9729cb3073620fbca6adfc"
+
+        def install
+          bin.install "formal"
+        end
     end
 end
   
